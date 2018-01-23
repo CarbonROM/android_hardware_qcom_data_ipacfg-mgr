2337dfbb2d7f1973645ee1cdefe029726e36e3db
gtpitch <bmontagu@gmail.com>
2018-01-22 22:57:34 -0500

    data: add guard
    This repo is used for Pixel 2 and Pixel 2XL currently
    so add guard to prevent CAF msm8998 from building it
    
    Change-Id: I429de513a627ef755f6f4b719be8fd444ee68bfc
    Signed-off-by: gtpitch <bmontagu@gmail.com>

--- Android.mk
+++ Android.mk
@@ -3,6 +3,8 @@ ifneq ($(TARGET_BOARD_AUTO),true)
     LOCAL_PATH := $(call my-dir)
 
     ifneq ($(filter msm8998,$(TARGET_BOARD_PLATFORM)),)
+    ifneq ($(BOARD_USES_QCOM_HARDWARE),true)
         include $(call first-makefiles-under,$(LOCAL_PATH)/msm8998)
+    endif
     endif
 endif
